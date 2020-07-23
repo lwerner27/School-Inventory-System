fc8834842649846c4a5670cfeb2ebfa03930e2b4
Lucas Werner <34554883+lwerner27@users.noreply.github.com>
2020-07-23 08:54:51 -0500

    Continued setup of the server.

--- server.js
+++ server.js
@@ -1,8 +1,33 @@
+// Imports
+const path = require("path");
+const bodyParser = require("body-parser");
 const express  =  require("express")
+const hbs = require("express-handlebars");
 
+// Variables
 const PORT  =  process.env.PORT || 8080
 
+// Express app setup
 const app =  express()
+
+// Handlebars configuration
+app.engine(
+    "handlebars",
+    hbs({ layoutsDir: "views/layouts/", defaultLayout: "main" })
+);
+app.set("view engine", "handlebars");
+app.set("views", "views");
+
+// Body Parser Setup
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Public Route
+app.use(express.static(path.join(__dirname, "public")));
+
+// 404 Route
+app.use((req, res) => {
+    res.status(404).render("404", { pageTitle: "404: Page Not Found" });
+});
 
 app.listen(PORT, () => {
     console.log("The server is listening on port: " + PORT)
