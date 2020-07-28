4e20f11d79e6b7189fd5513e22d824adf0d839d8
Lucas Werner <34554883+lwerner27@users.noreply.github.com>
2020-07-28 11:59:56 -0500

    Started on the landing page.

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,35 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className='App'>
+            <div className='container'>
+                <div className='row mt-5'>
+                    <div className='col-md-6 m-auto'>
+                        <div class='card card-body text-center'>
+                            <h1>
+                                <i className='fab fa-node-js fa-3x'></i>
+                            </h1>
+                            <p>Create Account or Login</p>
+                            <a
+                                href='/accounts/register'
+                                className='btn btn-primary btn-block mb-2'
+                            >
+                                Register
+                            </a>
+                            <a
+                                href='/accounts/login'
+                                className='btn btn-secondary btn-block mb-2'
+                            >
+                                Login
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
