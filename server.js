11063c1d30dc4006000877dbd7f9bad065d91777
Lucas Werner <34554883+lwerner27@users.noreply.github.com>
2020-07-28 12:41:02 -0500

    Removed handlebars views and configurations.

--- server.js
+++ server.js
@@ -1,20 +1,9 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const hbs = require("express-handlebars");
-const path = require("path");
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
 
 // Initialize Express App
 const app = express();
-
-app.engine(
-    "handlebars",
-    hbs({
-        layoutsDir: "views/layouts/",
-        defaultLayout: "main",
-    })
-);
-app.set("view engine", "handlebars");
-app.set("views", "views");
 
 app.use(
     bodyParser.urlencoded({
@@ -23,13 +12,13 @@ app.use(
 );
 
 // Public Static Route
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Routes
-app.use("/", require("./routes/index"));
+app.use('/', require('./routes/index'));
 
 const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, () => {
-    console.log("Server started on port: " + PORT);
+    console.log('Server started on port: ' + PORT);
 });
