fe913ec177b03f07b088b69e9a494ba8186266ba
Lucas Werner <34554883+lwerner27@users.noreply.github.com>
2020-07-28 11:15:11 -0500

    Formatting changes.

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
-const bodyParser = require("body-parser")
-const hbs = require("express-handlebars")
-const path = require("path")
+const bodyParser = require("body-parser");
+const hbs = require("express-handlebars");
+const path = require("path");
 
 // Initialize Express App
 const app = express();
@@ -10,24 +10,26 @@ app.engine(
     "handlebars",
     hbs({
         layoutsDir: "views/layouts/",
-        defaultLayout: "main"
+        defaultLayout: "main",
     })
 );
 app.set("view engine", "handlebars");
 app.set("views", "views");
 
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
+app.use(
+    bodyParser.urlencoded({
+        extended: true,
+    })
+);
 
 // Public Static Route
 app.use(express.static(path.join(__dirname, "public")));
 
 // Routes
-app.use("/", require("./routes/index"))
+app.use("/", require("./routes/index"));
 
 const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, () => {
-    console.log("Server started on port: " + PORT)
-})
+    console.log("Server started on port: " + PORT);
+});
